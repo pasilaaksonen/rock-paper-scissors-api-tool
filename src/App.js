72a5b4e20038b761b9801ae8bba9da08fa42a1a3
pasilaaksonen <pasi.laaksonen@edu.turkuamk.fi>
2021-12-27 12:55:15 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-
+import { useEffect, useState } from 'react';
+import historyServices from './services/history';
 
 function App() {
+
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    historyServices.getData().then((response) => {
+      setData(response.data)
+    })
+  }, [])
+
+  const handleHistoryCall = () => {
+    historyServices.history().then(response => {
+      console.log(response)
+    })
+  }
+
+  const handleUpdateData = () => {
+    historyServices.updateHistory().then(response => {
+      console.log("update data")
+    })
+  }
+
   return (
-    <>
-      <h1>Hello world!</h1>
-    </>
+    <div className="App">
+      <button onClick={handleHistoryCall}>Make a history call</button>
+      <button onClick={handleUpdateData}>Update data</button>
+    </div>
   );
 }
-
 export default App;

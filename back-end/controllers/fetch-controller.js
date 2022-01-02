5511be27a127d5e94e40c575d4ff4d00c63deaa6
pasilaaksonen <pasi.laaksonen@edu.turkuamk.fi>
2022-01-02 18:38:47 +0200

    Update fetch-controller.js

--- back-end/controllers/fetch-controller.js
+++ back-end/controllers/fetch-controller.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import UserData from '../models/user-model.js'
+import axios from 'axios';
 const router = express.Router();
 
 export const fetchHistory = async (req, res) => {
     UserData.find({}, (err, result) => {
         if (err) {
-          res.send(err);
+          res.send("Data not found");
         }
-        console.log(result[0].data)
+        // console.log(result[0].data)
         res.send(result[0]);
     });  
 }
